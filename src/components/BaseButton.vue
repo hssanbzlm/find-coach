799c9cb069cd9f92cca2e19b8ed6a8e789c3f926
Hassan Bouzlima <bouzlima.h@live.fr>
2023-01-09 16:42:27 +0100

    Add base components: button and chip :bricks:

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,25 @@
+
+<script setup lang="ts">
+const props = defineProps({
+    color:String
+})
+const emits = defineEmits(['clicked'])
+const onclick=()=>{
+     emits('clicked')
+}
+
+</script>
+
+<template>
+     <v-btn 
+        height="25"
+        @click="onclick"
+        rounded="lg"
+        :color="props.color"
+    >
+      <slot></slot>
+    </v-btn>
+</template>
+<style scoped>
+
+</style>
---
+++ src/components/BaseChip.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+    color:String,
+    textColor:String
+})
+        
+</script>
+
+<template>
+    <v-chip
+      class="ma-2"
+      :color="props.color"
+      :text-color="props.textColor"
+    >
+      <slot name="chip-text">
+      </slot>
+    </v-chip>
+</template>
+
+
+<style scoped>
+
+</style>
