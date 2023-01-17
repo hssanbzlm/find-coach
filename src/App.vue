be294302fda5a3c5e184a204d3e6c81b6cc74b4e
Hassan Bouzlima <bouzlima.h@live.fr>
2023-01-17 16:45:08 +0100

    Update store behavior :green_heart:

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useCoachesStore } from '@/stores/Coaches'
+const store = useCoachesStore()
+onMounted(() => {
+  store.fetchCoaches()
+})
+</script>
 <template>
   <v-app>
-    <router-view></router-view>
+    <router-view v-if="store.loaded"></router-view>
   </v-app>
 </template>
