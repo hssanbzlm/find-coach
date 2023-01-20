e578c156be698c40536caff4babb619bd6357cfe
Hassan Bouzlima <bouzlima.h@live.fr>
2023-01-20 17:48:25 +0100

    remove usesless code :wastebasket:

--- src/views/TheCoaches.vue
+++ src/views/TheCoaches.vue
@@ -29,14 +29,9 @@ const clickDetails = () => {
 const clickContact = (id: string) => {
   router.push({ name: 'contact', params: { coachId: id } })
 }
-const signOut = () => {
-  localStorage.removeItem('ID')
-  router.push({ name: 'Auth' })
-}
 </script>
 
 <template>
-  <div @click="signOut">signout</div>
   <CoachFilter @update-areas="updateArea" />
   <div v-for="coach in filtredCoaches" :key="coach.id">
     <CoachDetails
