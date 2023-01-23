97eaca1b16b42fa205b027334d16b511bbe0289f
Hassan Bouzlima <bouzlima.h@live.fr>
2023-01-23 17:59:39 +0100

    remove fetching coaches from App.vue :triangular_flag_on_post:

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { useCoachesStore } from '@/stores/Coaches'
 import { useUserStore } from './stores/User'
-const coachStore = useCoachesStore()
 const userStore = useUserStore()
-onMounted(() => {
-  coachStore.fetchCoaches()
-})
 </script>
 <template>
   <v-app>
-    <router-view
-      v-if="coachStore.loaded && userStore.isAuthChecked"
-    ></router-view>
-    <div v-else>loading</div>
+    <router-view v-if="userStore.isAuthChecked"></router-view>
+    <div v-else>Please wait some moments</div>
   </v-app>
 </template>
