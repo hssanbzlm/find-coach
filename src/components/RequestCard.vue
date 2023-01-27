555f600870e0401c677763346ff3dfad8da8570a
Hassan Bouzlima <bouzlima.h@live.fr>
2023-01-27 11:46:35 +0100

    Enhance request card display :art:

--- src/components/RequestCard.vue
+++ src/components/RequestCard.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-  <v-card>
+  <v-card class="card-width">
     <v-card-title class="bg-purple-lighten-2">
-      <h2 class="font-weight-light">
-        <slot name="coach-name"></slot>
-      </h2>
       <h5 class="font-weight-light">
-        <slot name="date"></slot>
+        <slot name="coach-name"></slot>
       </h5>
+      <h6 class="font-weight-light">
+        <slot name="date"></slot>
+      </h6>
     </v-card-title>
     <v-card-text>
       <slot name="message"></slot>
@@ -15,4 +15,8 @@
   </v-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-width {
+  width: 23rem;
+}
+</style>
