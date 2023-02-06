aec78239e58cfc60b63d0e46e64639f8097a5c47
Hassan Bouzlima <bouzlima.h@live.fr>
2023-02-06 17:10:29 +0100

    Add Requests store and refactor TheRequests component :recycle:

---
+++ src/stores/Requests.ts
@@ -0,0 +1,45 @@
+import type { IDataBase } from '@/db/IDataBase'
+import type { DataSnapshot } from 'firebase/database'
+import { defineStore } from 'pinia'
+import { inject } from 'vue'
+import { useUserStore } from './User'
+import type { Request } from '@/types/Request'
+export const useRequestStore = defineStore('requests', {
+  state: () => ({
+    requests: [] as Request[],
+    loaded: false,
+    error: false,
+    loading: false,
+  }),
+  getters: {
+    getRrequestState(state) {
+      return state
+    },
+  },
+  actions: {
+    fetchRequests() {
+      const appDataBase: IDataBase = inject('appDataBase')!
+      const { email } = useUserStore().getUser!
+      this.loaded = false
+      this.loading = true
+      console.log('appDataBase ', appDataBase)
+      appDataBase
+        .getRequests(email)
+        .then((snapshot: DataSnapshot) => {
+          if (snapshot.size > 0) {
+            snapshot.forEach((request) => {
+              this.requests.push(request.val())
+            })
+            this.requests.reverse()
+          }
+          this.loaded = true
+          this.loading = false
+        })
+        .catch(() => {
+          this.loaded = false
+          this.error = true
+          this.loading = false
+        })
+    },
+  },
+})
--- src/views/TheRequests.vue
+++ src/views/TheRequests.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useCoachesStore } from '@/stores/Coaches'
-import type { Request } from '@/types/Request'
 import RequestTimeline from '@/components/RequestTimeline.vue'
-import { useUserStore } from '@/stores/User'
+import { useRequestStore } from '@/stores/Requests'
 import { useDateFormat } from '@vueuse/core'
-import { inject } from 'vue'
-import type { IDataBase } from '@/db/IDataBase'
-import type { DataSnapshot } from '@firebase/database'
+import { storeToRefs } from 'pinia'
+import BaseButton from '@/components/BaseButton.vue'
+import BaseAlert from '@/components/BaseAlert.vue'
 const coacheStore = useCoachesStore()
-const userStore = useUserStore()
-const { email } = userStore.getUser!
-const appDataBase: IDataBase = inject('appDataBase')!
-
+const requestStore = useRequestStore()
 const getCoachDetails = (coachId: string) => {
   const { firstName, lastName } = coacheStore.getCoachById(coachId)
   return `${firstName} ${lastName}`
@@ -20,27 +15,24 @@ const getCoachDetails = (coachId: string) => {
 const getRequestTime = (timestamp: number) => {
   return useDateFormat(new Date(timestamp), 'DD-MM-YYYY hh:mm:ss A').value
 }
-const requests = ref<Request[]>([])
-const loading = ref(true)
-const error = ref(false)
-appDataBase
-  .getRequests(email)
-  .then((snapshot: DataSnapshot) => {
-    loading.value = false
-    if (snapshot.size > 0) {
-      snapshot.forEach((childSnapshot) => {
-        requests.value.push(childSnapshot.val())
-      })
-      requests.value.reverse()
-    }
-  })
-  .catch(() => {
-    loading.value = false
-    error.value = true
-  })
+const { requests, loading, error, loaded } = storeToRefs(requestStore)
+if (!loaded.value) {
+  loadContent()
+}
+function loadContent() {
+  requestStore.fetchRequests()
+}
 </script>
 <template>
-  <div v-if="!loading && !error">
+  <div v-if="loaded">
+    <BaseButton
+      class="mt-5 ml-5"
+      color="white"
+      textColor="black"
+      @Click="loadContent"
+    >
+      Refresh
+    </BaseButton>
     <v-timeline v-if="requests.length > 0">
       <RequestTimeline
         v-for="request in requests"
@@ -59,15 +51,18 @@ appDataBase
         </template>
       </RequestTimeline>
     </v-timeline>
-    <div class="d-flex justify-center mt-5" v-else>
+    <div v-else class="d-flex justify-center mt-5">
       <BaseAlert type="info" message="You have not sent any request yet" />
     </div>
   </div>
-  <div v-else>
-    <div v-if="loading">
-      <ProgressCircular />
-    </div>
-    <div v-if="error">Error</div>
+  <div v-else-if="loading">
+    <ProgressCircular />
+  </div>
+  <div v-else-if="error">
+    <BaseAlert
+      type="error"
+      message="Error while loading requests. Please come back later"
+    />
   </div>
 </template>
 <style scoped></style>
