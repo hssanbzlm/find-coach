b1b4ba26ede1281b81f724737d57eb02558ea9a7
Hassan Bouzlima <bouzlima.h@live.fr>
2023-08-01 16:24:03 +0100

    Enhance auth page view

--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -24,20 +24,40 @@ const signIn = () => {
 
 <template>
   <div class="auth-container">
-    <h1>Find your coach</h1>
-    <BaseButton @click="signIn" color="red" class="bt">
+    <div class="w-100 bg-deep-purple-lighten-5 text-center title-opacity">
+      <h1 class="text-font pa-4">Find your coach here !</h1>
+    </div>
+
+    <div
+      class="w-50 font-weight-bold pa-4 rounded-xl bg-deep-purple-lighten-5 description-opacity"
+    >
+      Our app is designed to help you connect with experienced coaches who can
+      help you achieve your goals. With our app, you can easily browse through a
+      wide range of coaches and find the one that best suits your needs. Our
+      powerful filtering system allows you to search for coaches by area of
+      expertise. Once you find a coach you're interested in, you can view their
+      detailed profile.
+    </div>
+    <BaseButton @click="signIn" color="red" class="text-font">
       <v-icon class="mr-5"> mdi-gmail </v-icon>
-      connect with gmail
+      Connect with gmail
     </BaseButton>
   </div>
 </template>
 
 <style scoped>
+.description-opacity {
+  opacity: 0.7;
+}
+.title-opacity {
+  opacity: 0.5;
+}
 .auth-container {
   height: 100vh;
   background: linear-gradient(
-      rgba(255, 255, 255, 0.7),
-      rgba(255, 255, 255, 0.1)
+      to right,
+      #000000 13%,
+      rgba(128, 128, 128, 0.1) 50%
     ),
     url('@/assets/signin-bg.jpg');
   background-size: cover;
@@ -45,5 +65,8 @@ const signIn = () => {
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+}
+.text-font {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 </style>
